be46d9e4bfba56ff0caff3a1ef99a533888f5fea
SuperLeven <levenhan@dingtalk.com>
2024-02-26 22:57:51 +0800

    fix: fixed file upload error

--- src/views/product/update-package/components/upload-card.vue
+++ src/views/product/update-package/components/upload-card.vue
@@ -1,88 +1,86 @@
 <template>
-  <n-upload
-    :action="url + '/file/up'"
-    :headers="{
-      'x-token': localStg.get('token') || ''
-    }"
-    :data="{ type: 'image' }"
-    :default-file-list="dataList"
-    :accept="accept"
-    :max="1"
-    @before-upload="beforeUpload"
-    @finish="handleFinish"
-    @error="handleError"
-  >
-    <n-button>{{ $t('page.product.update-package.package') }}</n-button>
-  </n-upload>
+	<n-upload :action="url + '/file/up'" :headers="{
+		'x-token': localStg.get('token') || ''
+	}" :data="{ type: 'image' }" :default-file-list="dataList" :accept="accept" :max="1" @before-upload="beforeUpload"
+		@finish="handleFinish" @error="handleError">
+		<n-button>{{ $t('page.product.update-package.package') }}</n-button>
+	</n-upload>
 </template>
 
 <script setup lang="ts">
-import type { Ref } from 'vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 // eslint-disable-next-line import/order
 import { getServiceEnvConfig } from '~/.env-config'
 const url = ref(getServiceEnvConfig(import.meta.env).url)
 defineOptions({ name: 'UploadFile' })
 // eslint-disable-next-line import/order
 import type { UploadFileInfo } from 'naive-ui'
-import { localStg } from '@/utils'
+import { generateUUID, getFileName, localStg } from '@/utils'
 import { STATIC_BASE_URL } from '~/src/config/config'
 import { $t } from '~/src/locales'
 export interface Props {
-  /**
-   * 选取文件的类型
-   */
-  accept: string
-  /** 上传的文件类型 */
-  fileType: string[]
-  value: string | null | undefined
+	/**
+	 * 选取文件的类型
+	 */
+	accept: string
+	/** 上传的文件类型 */
+	fileType: string[]
+	value: string | null | undefined
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  accept: 'file',
-  fileType: () => ['exe', 'apk', 'zip', 'ipa']
+	accept: 'file',
+	fileType: () => ['exe', 'apk', 'zip', 'ipa']
 })
-const dataList: Ref<UploadFileInfo[]> = ref(
-  props.value?.split(',').map(item => ({ url: item.replace('.', STATIC_BASE_URL) })) || []
-) as Ref<UploadFileInfo[]>
+const dataList = computed((): UploadFileInfo[] => {
+	if (!props.value) {
+		return []
+	}
+	return props.value?.split(',').map((item: string) => ({
+		id: generateUUID(),
+		name: getFileName(item),
+		status: 'finished',
+		url: item.replace('.', STATIC_BASE_URL)
+	})) || []
+})
 
 interface Emits {
-  (e: 'update:value', val: string): void
-  (e: 'success', file: UploadFileInfo): void
+	(e: 'update:value', val: string): void
+	(e: 'success', file: UploadFileInfo): void
 }
 
 const emit = defineEmits<Emits>()
 
 async function beforeUpload(data: { file: UploadFileInfo; fileList: UploadFileInfo[] }) {
-  let isImg: boolean = false
-  if (props.fileType.length) {
-    let fileExtension = ''
-    if (data.file?.name.lastIndexOf('.') > -1) {
-      fileExtension = data.file?.name.slice(data.file?.name.lastIndexOf('.') + 1)
-    }
-    isImg = props.fileType.some(type => {
-      if (data.file.file?.type && data.file.file?.type.indexOf(type) > -1) return true
-      if (fileExtension && fileExtension.indexOf(type) > -1) return true
-      return false
-    })
-  } else if (data.file.file?.type && data.file.file?.type.indexOf('image') > -1) {
-    isImg = true
-  }
-  if (!isImg) {
-    window.$message?.error(`文件格式不正确, 请上传${props.fileType.join('/')}图片格式文件!`)
-    return false
-  }
-  return true
+	let isImg: boolean = false
+	if (props.fileType.length) {
+		let fileExtension = ''
+		if (data.file?.name.lastIndexOf('.') > -1) {
+			fileExtension = data.file?.name.slice(data.file?.name.lastIndexOf('.') + 1)
+		}
+		isImg = props.fileType.some(type => {
+			if (data.file.file?.type && data.file.file?.type.indexOf(type) > -1) return true
+			if (fileExtension && fileExtension.indexOf(type) > -1) return true
+			return false
+		})
+	} else if (data.file.file?.type && data.file.file?.type.indexOf('image') > -1) {
+		isImg = true
+	}
+	if (!isImg) {
+		window.$message?.error(`文件格式不正确, 请上传${props.fileType.join('/')}图片格式文件!`)
+		return false
+	}
+	return true
 }
 
 function handleFinish({ file, event }: { file: UploadFileInfo; event?: ProgressEvent }) {
-  const response = JSON.parse((event?.target as XMLHttpRequest).response)
-  window.$message?.success(response.message)
-  emit('update:value', response.data.path)
-  emit('success', file)
+	const response = JSON.parse((event?.target as XMLHttpRequest).response)
+	window.$message?.success(response.message)
+	emit('update:value', response.data.path)
+	emit('success', file)
 }
 
 function handleError({ event }: { event?: ProgressEvent }) {
-  window.$message?.error((event?.target as XMLHttpRequest).response || '文件上传失败，请重试')
+	window.$message?.error((event?.target as XMLHttpRequest).response || '文件上传失败，请重试')
 }
 </script>

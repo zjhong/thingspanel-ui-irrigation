3e20bf13b122b4f5927e2519daa02e624eb666e0
Admin <wbhong@zjaq.com>
2024-03-01 22:11:28 +0800

    fix: Kanban related modifications Several bugs such as logging out and logging in

--- src/components/panel/index.ts
+++ src/components/panel/index.ts
@@ -1,5 +1,5 @@
-const builtinCards = import.meta.glob('./builtin-card/*/index.Vue');
-const chartCards = import.meta.glob('./chart-card/*/index.Vue');
+const builtinCards = import.meta.glob('./builtin-card/*/index.ts');
+const chartCards = import.meta.glob('./chart-card/*/index.ts');
 
 export const PanelCards = {
   builtin: Object.values(builtinCards).map((b: any) => b.default),

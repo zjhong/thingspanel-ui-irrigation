85a2ce79ac5b3321984c61c59e5a6120ebe89482
Wrong2014 <wbhong@zjaq.com>
2024-03-04 11:55:03 +0800

    fix: ....

--- src/views/device/grouping-details/index.vue
+++ src/views/device/grouping-details/index.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
+import { useMessage } from 'naive-ui';
 import { deviceGroupDetail, getDeviceGroup } from '@/service/api/device';
-
 const route = useRoute();
 const id = route.query.id;
-
+const message = useMessage();
 const geDetails = async () => {
-  const res = await deviceGroupDetail({ id });
-  console.log(res);
+  if (!id) {
+    message.error('没有传人的分组id');
+  } else {
+    const res = await deviceGroupDetail({ id });
 
-  const res2 = await getDeviceGroup({
-    page: 1,
-    page_size: 10,
-    parent_id: id
-  });
-  console.log(res2);
+    console.log(res);
+
+    const res2 = await getDeviceGroup({
+      page: 1,
+      page_size: 10,
+      parent_id: id
+    });
+    console.log(res2);
+  }
 };
 
 onMounted(geDetails);

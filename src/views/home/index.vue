4bb15e03d4ad7d6a1788a015427d08851876632b
Wrong2014 <wbhong@zjaq.com>
2024-03-19 11:47:30 +0800

    feat: home add

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import { router } from '@/router';
 import type { ICardView } from '@/components/panel/card';
 import { fetchHomeData } from '@/service/api';
 
 const layout = ref<ICardView[]>([]);
+const isError = ref<boolean>(false);
+const active = ref<boolean>(false);
+
 const getLayout = async () => {
   const { data, error } = await fetchHomeData({});
 
-  if (!error) {
-    if (data && data.config) {
-      const configJson = JSON.parse(data.config);
-      layout.value = [...configJson, ...layout.value];
-    }
+  isError.value = (error || !(data && data.config)) as boolean;
+
+  if (!isError.value && data) {
+    const configJson = JSON.parse(data.config);
+    layout.value = [...configJson, ...layout.value];
   }
-  console.log(data);
 };
 
 onMounted(getLayout);
 </script>
 
 <template>
+  <div v-if="isError" class="h-full w-full flex-center">
+    <n-result status="418" title="首页未配置或配置异常" description="一切尽在不言中">
+      <template #footer>
+        <n-button
+          type="primary"
+          :disabled="active"
+          @click="
+            () => {
+              router.push('/');
+              active = true;
+            }
+          "
+        >
+          <n-countdown
+            v-if="active"
+            :duration="15000"
+            :render="props => props.seconds + 's'"
+            :active="active"
+            @finish="active = false"
+          />
+          {{ active ? '' : '刷新' }}
+        </n-button>
+      </template>
+    </n-result>
+  </div>
+
   <CardRender
+    v-else
     ref="cr"
     v-model:layout="layout"
     :is-preview="true"

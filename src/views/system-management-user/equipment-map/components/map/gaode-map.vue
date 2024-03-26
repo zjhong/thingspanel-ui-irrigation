b82c39a58e08b5c01937811e2f1dd8b5ec2cd487
cao0707 <694367557@qq.com>
2024-03-26 18:20:15 +0800

    fix: Add Device Map Page

--- src/views/system-management-user/equipment-map/components/map/gaode-map.vue
+++ src/views/system-management-user/equipment-map/components/map/gaode-map.vue
@@ -81,16 +81,16 @@ export default {
               // 将创建的点标记添加到已有的地图实例：
               state.map.add(marker);
               //每次新建多边形前先把之前的删掉，避免覆盖。
-            //   if (state.lnglatArr.length > 1) {
-            //     state.map.remove(polygon);
-            //   }
+              if (state.lnglatArr.length > 1) {
+                state.map.remove(polygon);
+              }
               polygon = new AMap.Polygon({
                 path: state.lnglatArr, //多边形坐标
                 strokeColor: "#A0CFF6", //线颜色
                 strokeWeight: 1,
                 strokeOpacity: 1, //线透明度
                 fillOpacity: 0.4, //填充透明度
-                // fillColor: "#1791fc", //填充颜色
+                fillColor: "#1791fc", //填充颜色
                 zIndex: 50,
               });
               state.map.add(polygon); //生成多边形

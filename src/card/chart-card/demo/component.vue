14741de4ca696021b119e0ea6e3d95312066f671
Wrong2014 <wbhong@zjaq.com>
2024-03-29 20:30:49 +0800

    feat:  device template and kanban

--- src/card/chart-card/demo/component.vue
+++ src/card/chart-card/demo/component.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useWebSocket } from '@vueuse/core';
 import type { ICardData } from '@/components/panel/card';
+import { localStg } from '@/utils/storage';
 import icons from './icon';
+import { createServiceConfig } from '~/env.config';
+
 const iconMap = new Map(icons.map(c => [c.name, c.value]));
 // 正式环境可根据api获取
 const value = ref(1);
-defineProps<{
+const props = defineProps<{
   card: ICardData;
   // mode: IConfigCtx['view'];
 }>();
+console.log(props.card);
+const { otherBaseURL } = createServiceConfig(import.meta.env);
+let wsUrl = otherBaseURL.demo.replace('http', 'ws').replace('http', 'ws');
+
+const token = localStg.get('token');
+
+console.log(props.card, '325435435:', props?.card?.dataSource?.deviceSource?.[0]?.deviceId, token);
+if (props?.card?.dataSource?.deviceSource && props?.card?.dataSource?.deviceSource?.[0]?.deviceId) {
+  wsUrl += `/telemetry/datas/current/keys/ws`;
+  const keys = [props?.card?.dataSource?.deviceSource?.[0]?.metricsId];
+  const { status, data, send, open, close } = useWebSocket(wsUrl);
+  console.log(status, data, send, open, close, keys);
+}
 </script>
 
 <template>
--- src/components/panel/ui/add-card.vue
+++ src/components/panel/ui/add-card.vue
@@ -202,6 +202,7 @@ onMounted(() => {
               }
               availableCardIds = [];
               webChartConfig = [];
+              deviceSelectId = '';
               tabValue = value;
             }
           "

c507681a2b24650b8f30579d7f4e016e8ccc59de
wbh <1070750088@qq.com>
2024-04-01 22:03:19 +0800

    feat: test commit

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const a: number = 999;
 const s: string = '32';
-const c: number = a + 1;
+const c: number = a + 10;
 // const c: number = s
 console.log(a, c, s);
 </script>

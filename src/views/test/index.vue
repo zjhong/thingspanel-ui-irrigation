012fa1f74492a84e9663d07b52536fb9f57d17a3
Wrong2014 <wbhong@zjaq.com>
2024-04-18 14:27:19 +0800

    feat: Refactor Chart and Chart Data Selection Component and  other bugs

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import {ref, watch} from 'vue';
 import AggregationSelector from '@/views/device/details/modules/telemetry/modules/AggregationSelector.vue';
 import ChartComponent from './modules/ChartComponent.vue';
 
@@ -36,21 +36,28 @@ const refuse = () => {
 
 watch(
   selectedOption,
-  () => {
+  v => {
+
+
+    console.log(v.time_range);
+    if (v.time_range === 'custom' && (!v.start_time || !v.end_time)) {
+      window.NMessage.error('当时间为自定义时，必须选择开始和接受时间');
+      return;
+    }
     refuse();
     // 这里可以放置当 selectedOption 变化时需要执行的逻辑
   },
-  { deep: true }
+  {deep: true}
 );
 </script>
 
 <template>
   <div>
-    <AggregationSelector v-model:value="selectedOption" device_id="test_device_id" thekey="test_device_key" />
+    <AggregationSelector v-model:value="selectedOption" device_id="test_device_id" thekey="test_device_key"/>
     <NCard class="mt-18px w-600px">
-      <NCode word-wrap :code="JSON.stringify(selectedOption, null, 2)" language="json" />
+      <NCode word-wrap :code="JSON.stringify(selectedOption, null, 2)" language="json"/>
     </NCard>
 
-    <ChartComponent v-if="false" :initial-options="initialOptions" />
+    <ChartComponent v-if="false" :initial-options="initialOptions"/>
   </div>
 </template>

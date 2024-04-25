30790d73de8a4f8c4bb400974ecf39391c1c65f5
1531457828@qq.com <1531457828@qq.com>
2024-04-25 21:28:05 +0800

    fix: Equipment switch function module adjustment

--- src/card/chart-card/switch/api.ts
+++ src/card/chart-card/switch/api.ts
@@ -1,10 +1,5 @@
-import { request } from "@/service/request/index";
+import { request } from '@/service/request/index';
 
 export const deviceDatas = async (data: any) => {
-  return await request.post<any>("/telemetry/datas/pub", data);
-};
-
-// 获取当前设备的详情
-export const deviceDetails = async (data: any) => {
-  return await request.get<any>("/telemetry/datas/current/ws", data);
+  return await request.post<any>('/telemetry/datas/pub', data);
 };

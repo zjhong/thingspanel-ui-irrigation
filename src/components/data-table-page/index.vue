261267a63b8bce36eb3f1cfe3fb51b6a456db418
songren <732054167@qq.com>
2024-05-09 10:31:37 +0800

    feat(projects): update bug

--- src/components/data-table-page/index.vue
+++ src/components/data-table-page/index.vue
@@ -183,7 +183,7 @@ const onUpdatePageSize = newPageSize => {
 // 观察分页和搜索条件的变化，自动重新获取数据
 watchEffect(() => {
   searchConfigs.map((item: any) => {
-    if (item?.extendParams)
+    if (item?.extendParams && searchCriteria.value[item.key])
       item?.options.map(oitem => {
         item?.extendParams.map(eitem => {
           searchCriteria.value[eitem.label] = oitem[eitem.value];

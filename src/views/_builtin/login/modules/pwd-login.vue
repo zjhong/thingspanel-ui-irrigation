fe6c903ed5575faa3c8c2f6ae377c857d4f2d0aa
程沛沛 <872301800@qq.com>
2024-05-22 16:57:26 +0800

    fix(projects): fix

--- src/views/_builtin/login/modules/pwd-login.vue
+++ src/views/_builtin/login/modules/pwd-login.vue
@@ -57,7 +57,7 @@ onMounted(() => {
     isRememberPath.value = is_remember_rath === '1';
   }
   const data = localStorage.getItem('enableZcAndYzm') as string;
-  if (data !== undefined) {
+  if (data) {
     const param = JSON.parse(data);
     showZc.value = param.enable_reg;
     showYzm.value = param.use_captcha;
